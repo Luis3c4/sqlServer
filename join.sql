006a7dcaca41601a0ff6e2a4b84ff0d9c6629da3
luis3c4 <u20231587@utp.edu.pe>
2025-08-29 16:36:59 -0500

    query join

--- join.sql
+++ join.sql
@@ -4,11 +4,7 @@ create TABLE  tblTransaction (
 	EmployeeNumber int not null
 )
 
-INSERT INTO tblTransaction VALUES
-(-964.05, '20150526', 658), 
-(-105.23, '20150914', 987)
-
-select * from tblTransaction tt 
+select tt. from tblTransaction tt 
 
 select * from employee e 
 join tblTransaction tt 
