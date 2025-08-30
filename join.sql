fda1f9d03b64cf39ef0005d5875e42aacd5f3e38
luis3c4 <u20231587@utp.edu.pe>
2025-08-30 16:10:23 -0500

    ejecicio de join con delete tabla heredada y rollback

--- join.sql
+++ join.sql
@@ -3,7 +3,7 @@ create TABLE  tblTransaction (
 	DateOfTransaction smalldatetime NULL ,
 	EmployeeNumber int not null
 )
-
+go
 select * from tblTransaction tt 
 
 select e.EmployeeNumber,e.EmployeeFirstName, e.EmployeeLastName, 
@@ -12,3 +12,41 @@ from employee e
 left join tblTransaction tt 
 on e.EmployeeNumber = tt.EmployeeNumber
 GROUP by e.EmployeeNumber, e.EmployeeFirstName, e.EmployeeLastName
+go
+
+select td.Department,td.DepartmentHead , sum(tt.Amount) as SumOfAmount
+from tblDepartment td
+left join employee e
+on td.Department = e.Department
+join tblTransaction tt
+on tt.EmployeeNumber = tt.EmployeeNumber
+group by td.Department, td.DepartmentHead
+order by td.Department
+go
+
+insert into tblDepartment(Department, DepartmentHead) values ('Accounts', 'James')
+go
+
+-- join con delete y tabla heredada
+begin transaction
+select count(*) from tblTransaction
+delete tblTransaction 
+from tblTransaction tt
+where tt.EmployeeNumber in (
+	select newTable.TNumber from (
+	select e.EmployeeNumber AS ENumber, 
+	e.EmployeeFirstName,
+	e.EmployeeLastName,
+	t.EmployeeNumber as TNumber,
+	sum(t.Amount) as TotalAmount
+	from employee e
+	right join tblTransaction t
+	on e.EmployeeNumber = t.EmployeeNumber
+	group by e.EmployeeNumber, e.EmployeeFirstName, e.EmployeeLastName, t.EmployeeNumber
+) as newTable
+where newTable.ENumber is null
+)
+select count(*) from tblTransaction
+rollback tran
+select count(*) from tblTransaction
+
