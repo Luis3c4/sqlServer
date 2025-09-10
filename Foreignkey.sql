46cf0b0a5b1f2e948f81f5cecd47bae94afc41d8
luis3c4 <u20231587@utp.edu.pe>
2025-09-10 18:07:12 -0500

    foreign key

---
+++ Foreignkey.sql
@@ -0,0 +1,13 @@
+BEGIN TRAN
+ALTER TABLE tblTransaction alter column EmployeeNumber INT null
+
+ALTER TABLE tblTransaction add constraint DF_tblTransaction DEFAULT 124 FOR EmployeeNumber
+
+ALTER TABLE tblTransaction with nocheck --Esto sirve cuando ya tienes datos que no cumplen la integridad referencial y quieres crear la FK sin que falle.
+add constraint FK_tblTransaction_EmployeeNumber FOREIGN KEY (EmployeeNumber)
+References employee(EmployeeNumber)
+ON UPDATE cascade 
+
+	UPDATE employee Set EmployeeNumber = 9123 where EmployeeNumber = 123
+	
+ROLLBACK TRAN
